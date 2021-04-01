143432fc01a4ed89f765d90de2eb1b92e8bdef5f
sleepingsaint <suryasantosh14523@gmail.com>
2021-04-01 20:24:31 +0530

    initial commit

---
+++ scrape.py
@@ -0,0 +1,73 @@
+# python script to scrape google images for given search
+from selenium import webdriver
+import requests
+import uuid
+import time
+import os
+import argparse
+
+BASE_URL = "https://www.google.com/search?tbm=isch&q="
+
+search_input = "hot dog"
+
+def download_image(image, storage_path):
+    image_url = image.get_attribute("data-src")
+    
+    if image_url is None:
+        return
+
+    req = requests.get(image_url)
+    extension = req.headers["Content-Type"].split('/')[-1]
+    filename = storage_path + "/" + str(uuid.uuid1()) + "." + extension
+    with open(filename, "wb") as f:
+        f.write(req.content)     
+    
+def scrape(search_input, iter = 0):
+    query = "+".join(search_input.split(" "))
+    driver.get(BASE_URL + query)
+
+    page_scroll_sleep = 2
+
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(page_scroll_sleep)
+
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            if iter > 0:
+                try:
+                    element = driver.find_elements_by_class_name('mye4qd')
+                    element[0].click()
+                    time.sleep(page_scroll_sleep)
+                    new_height = driver.execute_script("return document.body.scrollHeight")
+                except:
+                    break
+            else:
+                break
+
+        last_height = new_height
+
+    images = driver.find_elements_by_class_name('rg_i.Q4LuWd')
+
+    storage_path = "images/" + "_".join(search_input.split(" "))
+    
+    if not os.path.exists(storage_path):
+        os.makedirs(storage_path)
+
+    for img in images:
+        download_image(img, storage_path)
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description="command line python script to scrape google images")
+    arg_parser.add_argument("-s", "--search", type=str, nargs="*", help="python3 scrape.py -s coke 'hot dog'")
+    args = arg_parser.parse_args()
+
+    if len(args.search) == 0:
+        quit()
+
+    driver = webdriver.Firefox()
+    for s in args.search:
+        scrape(s)
